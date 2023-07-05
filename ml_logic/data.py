ab57f97eea6d7e4c467c714580b07cd05676d93b
pedro abi <pedro.abi.samara@gmail.com>
2023-07-05 08:54:33 -0300

    Initiated encoding the features, by first selecting which encoder to use in each feature

--- ml_logic/data.py
+++ ml_logic/data.py
@@ -2,9 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.preprocessing import RobustScaler, MinMaxScaler, StandardScaler, LabelEncoder
 
 
 data = pd.read_csv('../data/WA_Fn-UseC_-Telco-Customer-Churn.csv')
+
+# ------- DATA REVIEW -------
 
 data.columns
 data.head()
@@ -13,9 +16,6 @@ data.dtypes
 data.info()
 data.describe()
 data.isna().sum()
-
-# convert the 'TotalCharges' column to a numeric data type.
-data['TotalCharges'] = pd.to_numeric(data['TotalCharges'], errors='coerce')
 
 data['gender'].value_counts(normalize=True)
 data['SeniorCitizen'].value_counts(normalize=True)
@@ -27,14 +27,13 @@ data['Churn'].value_counts(normalize=True)
 for col in data.columns:
     print(col, data[col].unique())
 
+# ------- EXPLORATORY DATA ANALYSIS -------
+
 # plot a histogram of the 'tenure' column using seaborn
-sns.distplot(data['tenure'], kde=False, bins=30);
+sns.histplot(data['tenure'], kde=True, bins=30);
 
 # plot a histogram of the 'MonthlyCharges' column using seaborn
-sns.distplot(data['MonthlyCharges'], kde=False, bins=30);
-
-# plot a histogram of the 'TotalCharges' column using seaborn
-sns.distplot(data['TotalCharges'], kde=False, bins=30);
+sns.histplot(data['MonthlyCharges'], kde=True, bins=30);
 
 # Explorative Data Analysis on 'Churn' column
 sns.countplot(data['Churn']);
@@ -59,3 +58,24 @@ for i, col in enumerate(cols):
     ax = plt.subplot(1, len(cols), i+1)
     sns.countplot(x ="Churn", hue = str(col), data = data)
     ax.set_title(f"{col}")
+
+# identify ouutlier in the tenure column
+sns.boxplot(data['tenure'])
+
+# identify ouutlier in the MonthlyCharges column
+sns.boxplot(data['MonthlyCharges'])
+
+# ------- DATA CLEANING AND PREPROCESSING -------
+
+# convert the 'TotalCharges' column to a numeric data type.
+data['TotalCharges'] = data['TotalCharges'].apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
+
+# analyzing only the categorical features of 'data'
+cat_features = data.select_dtypes(include='object')
+cat_features.head()
+
+# setting each categorical feature to its proper encoder
+ohe_features = ['gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity']
+oe_features = ['OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod']
+le_features = ['Churn']
+le = LabelEncoder()
